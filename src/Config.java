025cff39a7ad478af9a37ab7bb2806d1211fe3ee
ZeroBound <767483128@qq.com>
2019-09-08 21:24:30 +0800

    update src

---
+++ src/Config.java
@@ -0,0 +1,27 @@
+/**
+ * @author zzw
+ */
+
+public enum Config {
+    /**/
+    L_TiME(5000), M_TIME(3000),S_TIME(1000);
+
+    private final int timeout;
+
+    Config (int timeout){
+        this.timeout = timeout;
+    }
+
+    public int getTime(){
+        String windowsOS = "Windows 7";
+        if(windowsOS.equals(getOS())){
+            return this.timeout*2;
+        }else{
+            return this.timeout;
+        }
+    }
+
+    private String getOS(){
+        return System.getProperty("os.name");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+/**
+ * @author zzw
+ */
 public class Main {
-
+    private static final int time = Config.L_TiME.getTime();
     public static void main(String[] args) {
+        System.out.println(new Main());
+        System.out.println(time);
         System.out.println("Hello World!");
     }
 }
